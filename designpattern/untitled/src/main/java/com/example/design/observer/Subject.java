b999b85b67fc3e23f2a68816c9a1bc2099d55733
yeoeol <gnsl3528@naver.com>
2025-11-29 21:25:50 +0900

    옵저버 패턴 구현

---
+++ designpattern/untitled/src/main/java/com/example/design/observer/Subject.java
@@ -0,0 +1,8 @@
+package com.example.design.observer;
+
+public interface Subject {
+    public void register(Observer obj);
+    public void unregister(Observer obj);
+    public void notifyObservers();
+    public Object getUpdate(Observer obj);
+}
